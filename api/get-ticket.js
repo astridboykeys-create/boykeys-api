0c776e8ff8808d6b66f190f4d45c03a900329295
astridboykeys-create <astridboykeys@gmail.com>
2026-06-08 11:44:40 +0200

    Update get-ticket.js

--- api/get-ticket.js
+++ api/get-ticket.js
@@ -1,107 +1,27 @@
 export default async function handler(req, res) {
 
-  const requestId = req.query.request_id;
+  // CORS
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
 
-  if (!requestId) {
-    return res.status(400).json({
-      error: "request_id ontbreekt"
-    });
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "*"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
   }
 
-  try {
-
-    // Ticket zoeken
-    const ticketResponse = await fetch(
-      "https://api.hubapi.com/crm/v3/objects/tickets/search",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization:
-            `Bearer ${process.env.HUBSPOT_TOKEN}`
-        },
-        body: JSON.stringify({
-          filterGroups: [
-            {
-              filters: [
-                {
-                  propertyName: "request_id",
-                  operator: "EQ",
-                  value: requestId
-                }
-              ]
-            }
-          ],
-          properties: [
-            "beschikbare_fotografen"
-          ],
-          limit: 1
-        })
-      }
-    );
-
-    const ticketData =
-      await ticketResponse.json();
-
-    if (
-      !ticketData.results ||
-      ticketData.results.length === 0
-    ) {
-      return res.status(404).json({
-        error: "Ticket niet gevonden"
-      });
-    }
-
-    const ticket =
-      ticketData.results[0];
-
-    const ids =
-      (ticket.properties.beschikbare_fotografen || "")
-        .split(";")
-        .map(id => id.trim())
-        .filter(Boolean);
-
-    const photographers = [];
-
-    // Fotografen ophalen
-    for (const id of ids) {
-
-      const contactResponse =
-        await fetch(
-          `https://api.hubapi.com/crm/v3/objects/contacts/${id}?properties=firstname,lastname,regio`,
-          {
-            headers: {
-              Authorization:
-                `Bearer ${process.env.HUBSPOT_TOKEN}`
-            }
-          }
-        );
-
-      const contact =
-        await contactResponse.json();
-
-      photographers.push({
-        id,
-        firstname:
-          contact.properties?.firstname || "",
-        lastname:
-          contact.properties?.lastname || "",
-        regio:
-          contact.properties?.regio || ""
-      });
-
-    }
-
-    return res.status(200).json({
-      photographers
-    });
-
-  } catch (error) {
-
-    return res.status(500).json({
-      error: error.message
-    });
-
-  }
+  return res.status(200).json({
+    cors: "werkt"
+  });
 
 }
