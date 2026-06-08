089632c73ebf55f631e3ebeafae30d04a9919c67
astridboykeys-create <astridboykeys@gmail.com>
2026-06-08 11:17:59 +0200

    Update get-ticket.js

--- api/get-ticket.js
+++ api/get-ticket.js
@@ -1,5 +1,28 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "API werkt!"
-  });
+export default async function handler(req, res) {
+
+  try {
+
+    const response = await fetch(
+      "https://api.hubapi.com/crm/v3/objects/tickets?limit=1",
+      {
+        headers: {
+          Authorization:
+            `Bearer ${process.env.HUBSPOT_TOKEN}`
+        }
+      }
+    );
+
+    const data =
+      await response.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
 }
