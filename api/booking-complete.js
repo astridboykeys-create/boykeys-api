539846a74f5c821f01949327ac2a14efb719f4d2
astridboykeys-create <astridboykeys@gmail.com>
2026-06-11 12:26:36 +0200

    Update booking-complete.js

--- api/booking-complete.js
+++ api/booking-complete.js
@@ -3,6 +3,32 @@ export default async function handler(
   res
 ) {
 
+  // CORS
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST,OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "*"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed"
+    });
+  }
+
   try {
 
     console.log(
