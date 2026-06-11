c134ebfd7b3085546433acb69373bb720995b766
astridboykeys-create <astridboykeys@gmail.com>
2026-06-11 15:41:08 +0200

    Update can-book.js

--- api/can-book.js
+++ api/can-book.js
@@ -1,23 +1,42 @@
-const bestaandeStart =
-  new Date(
-    "2026-06-17T17:00:00Z"
-  );
+export default async function handler(
+  req,
+  res
+) {
 
-const bestaandeEinde =
-  new Date(
-    "2026-06-17T19:00:00Z"
-  );
+  const bestaandeStart =
+    new Date(
+      "2026-06-17T17:00:00Z"
+    );
 
-const nieuweStart =
-  new Date(
-    "2026-06-17T18:00:00Z"
-  );
+  const bestaandeEinde =
+    new Date(
+      "2026-06-17T19:00:00Z"
+    );
 
-const nieuweEinde =
-  new Date(
-    "2026-06-17T20:00:00Z"
-  );
+  const nieuweStart =
+    new Date(
+      "2026-06-17T18:00:00Z"
+    );
 
-const overlap =
-  nieuweStart < bestaandeEinde &&
-  nieuweEinde > bestaandeStart;
+  const nieuweEinde =
+    new Date(
+      "2026-06-17T20:00:00Z"
+    );
+
+  const overlap =
+    nieuweStart < bestaandeEinde &&
+    nieuweEinde > bestaandeStart;
+
+  return res.status(200).json({
+
+    bestaandeStart,
+    bestaandeEinde,
+
+    nieuweStart,
+    nieuweEinde,
+
+    overlap
+
+  });
+
+}
