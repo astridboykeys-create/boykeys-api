644d70af95116a9d91e337df86f293a63ac116d7
astridboykeys-create <astridboykeys@gmail.com>
2026-06-11 14:18:53 +0200

    Create can-book.js

---
+++ api/can-book.js
@@ -0,0 +1,19 @@
+const bestaandeStart =
+  new Date(
+    "2026-06-17T17:00:00Z"
+  );
+
+const bestaandeEinde =
+  new Date(
+    "2026-06-17T19:00:00Z"
+  );
+
+const nieuweStart =
+  new Date(
+    "2026-06-17T18:00:00Z"
+  );
+
+const nieuweEinde =
+  new Date(
+    "2026-06-17T20:00:00Z"
+  );
