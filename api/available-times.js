79d63c7789ba99dff58722da8319ac6263225d5d
astridboykeys-create <astridboykeys@gmail.com>
2026-06-19 16:28:33 +0200

    Update available-times.js

--- api/available-times.js
+++ api/available-times.js
@@ -1,1 +1,11 @@
+import { enableCors } from "../lib/cors.js";
 
+export default async function handler(req, res) {
+
+  if (enableCors(req, res)) return;
+
+  return res.status(200).json({
+    success: true
+  });
+
+}
