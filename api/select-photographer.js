e4aa411d9a0144b6d44f73a6a5dd1076114c9a5f
astridboykeys-create <astridboykeys@gmail.com>
2026-06-19 13:19:14 +0200

    Update select-photographer.js

--- api/select-photographer.js
+++ api/select-photographer.js
@@ -1,3 +1,5 @@
+import { enableCors } from "../lib/cors.js";
+
 import {
   searchTickets,
   getTicketAssociations,
@@ -7,25 +9,7 @@ import {
 
 export default async function handler(req, res) {
 
-  // CORS
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "*"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST,OPTIONS"
-  );
-
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "*"
-  );
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
+if (enableCors(req, res)) return;
 
   if (req.method !== "POST") {
     return res.status(405).json({
