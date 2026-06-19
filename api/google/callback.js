651959e952a266fba021cc2c52fa426e4f7fe0d4
astridboykeys-create <astridboykeys@gmail.com>
2026-06-19 21:17:31 +0200

    Create callback.js

---
+++ api/google/callback.js
@@ -0,0 +1,15 @@
+import { enableCors } from "../lib/cors.js";
+
+export default async function handler(req, res) {
+
+  if (enableCors(req, res)) return;
+
+  return res.status(200).json({
+
+    success: true,
+
+    query: req.query
+
+  });
+
+}
