9c364891ba3fe8a648a21f95740ea1ba801631b3
astridboykeys-create <astridboykeys@gmail.com>
2026-06-19 16:29:42 +0200

    Update available-times.js

--- api/available-times.js
+++ api/available-times.js
@@ -4,8 +4,40 @@ export default async function handler(req, res) {
 
   if (enableCors(req, res)) return;
 
+  const {
+    photographer_id,
+    date,
+    latitude,
+    longitude,
+    duration = 60
+  } = req.query;
+
+  if (
+    !photographer_id ||
+    !date ||
+    !latitude ||
+    !longitude
+  ) {
+
+    return res.status(400).json({
+      error:
+        "photographer_id, date, latitude en longitude zijn verplicht"
+    });
+
+  }
+
   return res.status(200).json({
-    success: true
+
+    photographer_id,
+
+    date,
+
+    latitude,
+
+    longitude,
+
+    duration
+
   });
 
 }
