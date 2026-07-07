1977485fcd4e41ae03cf48c965b22fcea370c872
astridboykeys-create <astridboykeys@gmail.com>
2026-07-07 11:30:37 +0200

    Update search-slots.js

--- api/search-slots.js
+++ api/search-slots.js
@@ -4,8 +4,13 @@ import {
   getAvailability,
   getBlocks
 } from "../lib/supabase.js";
+import { enableCors } from "../lib/cors.js";
 
 export default async function handler(req, res) {
+
+    if (enableCors(req, res)) {
+    return;
+  }
 
   if (req.method !== "POST") {
     return res.status(405).json({
